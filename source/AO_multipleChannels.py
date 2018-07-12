2778b2f1d6548a1b386fbdb3c6c2c83f8fdbe7b0
Yawei Hu <37615956+yawhuu@users.noreply.github.com>
2018-07-12 10:57:05 +0800

    Modified comments and reorganized code.
    Modified comments based on Joe Peck's feedback on AI example.

--- source/AO_multipleChannels.py
+++ source/AO_multipleChannels.py
@@ -1,34 +1,45 @@
 """
-NI ELVIS III Analog Output (AO) 1 Sample Example - Multiple Channels
-This example illustrates how to write values to multiple AO channels on the NI
-ELVIS III. To create an AO session, you need to define two parameters: bank
-and channel. Both are required parameters.
+NI ELVIS III Single Point, Multiple Channels, Analog Output Example
+This example illustrates how to write values to multiple analog output (AO)
+channels on the NI ELVIS III. The program first defines the configuration for
+the AO channels, then writes to the AO channels in a loop. Each time the write
+is called a list of single point data is written to the channels. The time
+between writes is not precisely timed, and is controlled by a software delay.
+
+The AO configuration consists of two parameters: bank and channel. There are
+two identical banks of AI channels (A and B). Each bank contains 2 analog
+output channels. Every channel can be configured for single ended mode.
+
+This example uses:
+    1. Bank A, Channel AO0.
+    2. Bank A, Channel AO1.
+
+Hardware setup:
+    No hardware is needed.
 
 Result:
     The program writes a values into both AO0 and AO1 on bank A.
 """
 import time
 import academicIO
-from enums import Bank, AOChannel, AIRange
+from enums import Bank, AOChannel
 
 # specify the bank, channels, and range for the AO session
 bank = Bank.A
 channel0 = AOChannel.AO0
 channel1 = AOChannel.AO1
-airange = AIRange.PLUS_OR_MINUS_10V
 
 # open an AO session and set the initial values for the parameters
 with academicIO.AnalogOutput({'bank': bank,     # define first channel: AO0
-                                        'channel': channel0},
-                                       {'bank': bank,     # define second channel: AO1
-                                        'channel': channel1,
-                                        'range': airange}) as AO_multiple_channels:
-    # The program writes values 20 times
+                              'channel': channel0},
+                             {'bank': bank,     # define second channel: AO1
+                              'channel': channel1}) as AO_multiple_channels:
+    # write to the AO channels 20 times
     for i in range(0, 20):
-        # delay for 0.001 seconds so that the program does not run too fast
-        time.sleep(0.001)
-
         # define the value as a floating-point number
         input_value = 3.5
         # write 3.5 to both AO0 and AI1 on bank A
         AO_multiple_channels.write(input_value)
+
+        # add a short delay before acquiring next data point
+        time.sleep(0.001)
--- source/AO_singleChannel.py
+++ source/AO_singleChannel.py
@@ -1,8 +1,20 @@
 """
-NI ELVIS III Analog Output (AO) 1 Sample Example - Single Channel
-This example illustrates how to write values to an AO channel on the NI ELVIS
-III. To create an AO session, you need to define two parameters: bank and
-channel. Both are required parameters.
+NI ELVIS III Single Point, Single Channel, Analog Output Example
+This example illustrates how to write values to an analog output (AO)
+channel on the NI ELVIS III. The program first defines the configuration for
+the AO channel, then writes to the AO channel in a loop. Each time the write
+is called a single point of data is written to the channel. The time between
+writes is not precisely timed, and is controlled by a software delay.
+
+The AO configuration consists of two parameters: bank and channel. There are
+two identical banks of AI channels (A and B). Each bank contains 2 analog
+output channels. Every channel can be configured for single ended mode.
+
+This example uses:
+    Bank A, Channel AI0.
+
+Hardware setup:
+    No hardware is needed.
 
 Result:
     The program writes values to AO0 on bank A.
@@ -17,14 +29,14 @@ channel = AOChannel.AO0
 
 # open an AO session and set the initial values for the parameters
 with academicIO.AnalogOutput({'bank': bank,
-                                        'channel': channel}) as AO_single_channel:
+                              'channel': channel}) as AO_single_channel:
 
-    # The program writes values 20 times
+    # write to the AO channel 20 times
     for i in range(0, 20):
-        # delay for 0.001 seconds so that the program does not run too fast
-        time.sleep(0.001)
-
         # define the value as a floating-point number
         input_value = 2.0
         # write 2.0 to AO0 on bank A
         AO_single_channel.write(input_value)
+
+        # add a short delay before acquiring next data point
+        time.sleep(0.001)
