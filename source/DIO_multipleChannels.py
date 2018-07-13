b15fb0c65749e1b7b2aaf817739a3c45c023d8fc
Yawei Hu <37615956+yawhuu@users.noreply.github.com>
2018-07-13 09:21:34 +0800

    Modified DIO comments based on style in AI example given from Joe Peck.
    https://rally1.rallydev.com/#/207578024888ud/detail/task/232403575780

--- source/DIO_multipleChannels.py
+++ source/DIO_multipleChannels.py
@@ -1,8 +1,24 @@
 """
-NI ELVIS III Digital Input and Output (DIO) Example - Multiple Channels
+NI ELVIS III Single Point, Multiple Channels, Digital Input and Output Example
 This example illustrates how to write values to and read values from multiple
-digital input and output channels. To create a DIO session, you need to define 
-bank, which is an optional parameter. The default value of bank is A.
+digital input and output (DIO) channel. The program first defines the
+configuration for the DIO channels, then reads and writes the DIO channels in
+a loop. Each time the write is called a list of data is written to the
+channels; each time the read is called a lsit of data is returned for the
+channels. The time between writes and reads is not precisely timed and is
+controlled by a software delay.
+
+The DIO configuration consists of one parameter: bank. There are two identical
+banks of DIO channels (A and B). Each bank contains 19 digital input and
+output channels. Each DIO channel contains two directions: write and read. The
+NI ELVIS III helper library (academicIO.py) will change the direction based on
+the function is called.
+
+This example uses:
+    Bank A, Channel DIO2, write direction.
+    Bank A, Channel DIO4, read direction.
+    Bank A, Channel DIO3, write direction.
+    Bank A, Channel DIO8, read direction.
 
 Hardware setup:
     1. Connect DIO2 to DIO4 on bank A.
@@ -25,16 +41,16 @@ with academicIO.DIO(bank) as DIO:
     channel3 = DIOChannel.DIO3
     channel4 = DIOChannel.DIO4
     channel8 = DIOChannel.DIO8
-    # specify the input value
+    # define the value as a boolean
     value = False
-    # The program generates and reads values 20 times
+    # writes to and reads from the DIO channels 20 times
     for x in range(0, 20):
-        # write values to DIO2 and DIO3 on bank A
+        # write the value False to both DIO2 and DIO3 on bank A
         DIO.write(value, channel2, channel3)
         # read values from DIO4 and DIO8 on bank A
         data = DIO.read(channel4, channel8)
         # the values read are [0, 0]
         print data
 
-        # delay for 0.001 seconds so that the program does not run too fast
+        # add a short delay before acquiring next data point
         time.sleep(0.001)
--- source/DIO_singleChannel.py
+++ source/DIO_singleChannel.py
@@ -1,8 +1,21 @@
 """
-NI ELVIS III Digital Input and Output (DIO) Example - Single Channel
+NI ELVIS III Single Point, Single Channel, Digital Input and Output Example
 This example illustrates how to write values to and read values from a single
-digital input and output channel. To create a DIO session, you need to define
-bank, which is an optional parameter. The default value of bank is A.
+digital input and output (DIO) channel. The program first defines the
+configuration for the DIO channels, then reads and writes the DIO channel in a
+loop. Each time the write is called a data is written to the channel; each
+time the read is called a data is returned for the channel. The time between
+reads is not precisely timed and is controlled by a software delay.
+
+The DIO configuration consists of one parameter: bank. There are two identical
+banks of DIO channels (A and B). Each bank contains 19 digital input and
+output channels. Each DIO channel contains two directions: write and read. The
+NI ELVIS III helper library (academicIO.py) will change the direction based on
+the function is called.
+
+This example uses:
+    Bank A, Channel DIO2, write direction.
+    Bank A, Channel DIO4, read direction.
 
 Hardware setup:
     Connect DIO2 to DIO4 on bank A.
@@ -21,16 +34,16 @@ with academicIO.DIO(bank) as DIO:
     # specify the DIO channels
     channel2 = DIOChannel.DIO2
     channel4 = DIOChannel.DIO4
-    # specify the input value
+    # define the value as a boolean
     value = True
-    # The program generates and reads values 20 times
+    # writes to and reads from the DIO channels 20 times
     for x in range(0, 20):
-        # write value to DIO2 on bank A
+        # write the value True to DIO2 on bank A
         DIO.write(value, channel2)
         # read value from DIO4 on bank A
         data = DIO.read(channel4)
-        # the value read is [0]
+        # the value read is [1]
         print data
 
-        # delay for 0.001 seconds so that the program does not run too fast
+        # add a short delay before acquiring next data point
         time.sleep(0.001)
