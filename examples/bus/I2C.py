6d029e4f75265ee73cc15c47b975a3e3a693c47e
Yawei Hu <37615956+yawhuu@users.noreply.github.com>
2018-07-20 10:36:41 +0800

    US191347: Modified comments for I2C and DIIRQ based on Joe Peck's feedback.
    https://rally1.rallydev.com/#/207578024888ud/detail/userstory/236765725448

--- examples/bus/I2C.py
+++ examples/bus/I2C.py
@@ -12,41 +12,34 @@ identical banks of I2C port (A and B). There are two speeds of I2C
 communication (standard and fast).
 
 To read data from and write data to the I2C slave device, you must specify an
-address in 7 bits. Some I2C devices might have an 8-bit address in which the
-first 7 bits represent the address and the last bit represents the mode of
-operation. For this kind of I2C devices, you must specify the slave address
-(7-bit) using the seven most significant bits (MSB). For example, a device
-provides a 8-bit slave address 10100110. You can determine it to '1010011 0'
-which has a 7-bit slave address 1010011. The last bit (0) is used to determine
-either to write to the slave device or read from the slave device. The NI
-ELVIS III helper library (academicIO) will insert the read/write bit based on
-the function been called.
+address in 7 bits. The NI ELVIS III helper library (academicIO) will left
+shift the address and insert the read/write bit based on the function been
+called to match the I2C standard. After that, it will become a 8 bits address.
+The first 7 bits represent the device address and the last bit represents the
+mode operation (read mode or write mode).
 
 See https://www.nxp.com/docs/en/user-guide/UM10204.pdf for more details about
 I2C.
 
-slave_device_address is an address which refers to the slave device. The slave
-device address is 0x53 which specifies to a 7-bit I2C connection address of
-ADXL345. You might need to change the slave device address depends on the
-device you have. This example illustrates how to set the ADXL345 device to
-measure mode by setting the power control register (0x2D).
+This example illustrates how to set the ADXL345 device to measure mode by
+setting the power control register (0x2D).
 
 See http://www.analog.com/media/en/technical-documentation/data-sheets/ADXL345.pdf
-for more details about ADXL345. See page.25 for more details about power
+for more details about ADXL345. See page 25 for more details about power
 control register.
 
 This example uses:
-    1. Bank A, Channel DIO14.
-    2. Bank A, Channel DIO15.
+    1. Bank A, I2C.SCL.
+    2. Bank A, I2C.SDA.
 
 Hardware setup:
     1. Connect an I2C.SCL of a slave device to I2C.SCL (DIO14) on bank A.
     2. Connect an I2C.SDA of a slave device to I2C.SDA (DIO15) on bank A.
 
 Result:
-    The program sets the I2C device to measure mode and reads a value from the
-    I2C slave device. The slave device returns the value that just be written
-    into the register. The returned value is 8 in decimal.
+    The program sets the I2C device to measure mode and reads back the value
+    from the same register of the I2C slave device for validation. The
+    returned value should be 8 in decimal.
 """
 import time
 import academicIO
@@ -60,15 +53,17 @@ speed = I2CSpeedMode.STANDARD
 
 # open an I2C session
 with academicIO.I2C(bank, speed) as I2C:
-    # specify the address, in hexadecimal, using the seven most significant
-    # bits (MSB) of the I2C slave device
+    # specify the 7-bit address, in hexadecimal
+    # we use the chip ADXL345 for this example and its slave device address is
+    # 0x53. You might need to change the slave device address depends on the
+    # device you have.
     slave_device_address = 0x53
 
     # specify the bytes to write to the I2C slave device
     # the first parameter of the write function is 0x2D which represents to
     # the power control register of ADXL345, and the second parameter of the
     # write function is the value to write to the register. 0x08 sets the
-    # third bit of the power control register to high which sets the device to
+    # bit 3 of the power control register to high which sets the device to
     # measure mode.
     data_to_write = [0x2D, 0x08]
     # write data to the I2C slave device
--- examples/interrupt/DIIRQ.py
+++ examples/interrupt/DIIRQ.py
@@ -5,41 +5,44 @@ the NI ELVIS III. The program first defines the configuration for the DI IRQ,
 then waits for an appropriate digital signal. When the DI channel receive the
 signal, the irq_handler function will be called.
 
-The DI IRQ configuration consists of one parameter: irq_channel. There is one
-identical bank of DI IRQ channels: A. Bank A contains 4 digital input
-interrupt channels (DIO0 to DIO3).
+The DI IRQ configuration consists of one parameter: irq_channel. Only 4
+channels support DI IRQ configuration, which are DIO0 to DIO3 on bank A.
 
 The configure function consists six parameters: irq_handler, irq_number,
 timeout, interrupt_type_rising, interrupt_type_falling, and edge_count.
 
-irq_handler is a required parameter. It defines the callback function which
-you use to handle interrupts. The callback function executes when the
-interrupt occurs. You can customize the callback function as needed. For
-example, you can write code to make an LED flash as shown in this example, or
-to read from an AI channel.
+irq_handler defines the callback function which you use to handle interrupts.
+The callback function executes when the interrupt occurs. You can customize
+the callback function as needed. For example, you can write code to make an
+LED flash as shown in this example, or to read from an AI channel.
 
 irq_number specifies the identifier of the interrupt to register. There are
-seven identical numbers of IRQ number (IRQ1 to IRQ8). You cannot register an
-I/O interrupt with the same IRQ number as that of a registered I/O interrupt.
-However, after you close the existing interrupt, you can use the IRQ number to
-register another interrupt.
+8 IRQ numbers (IRQ1 to IRQ8). You cannot register an I/O interrupt with the
+same IRQ number as that of a registered I/O interrupt. However, after you
+close the existing interrupt, you can use the IRQ number to register another
+interrupt.
 
 This example uses:
     Bank A, Channel DIO0.
 
 Hardware setup:
-    Connect a digital signal source to DIO0 on bank A. Gives an appropriate
-    digital signal before the program ends. You can connect BTN0 to DIO0 on
-    bank A to trigger the interrupt as indicated in this table:
+    Connect a digital signal source to DIO0 on bank A. Gives a digital signal
+    to match the interrupt conditions we set in the configuration before the
+    timeout expires. You can connect BTN0 to DIO0 on bank A to trigger the
+    interrupt as indicated in this table:
         1. Connect BTN0 A to DIO0 on bank A, then connect to a 10k Ohm
            resistance.
         2. Connect a +3.3 V voltage source to the 10k Ohm resistance.
         3. Connect BTN0 B to DGND.
-    Then, press the button BTN0.
+    Then, press the button BTN0. The interrupt is trigger when you press the
+    button.
 
 Result:
-    An interrupt occurs when DIO0 receives an appropriate digital signal.
-    The program calls irq_handler when the interrupt occurs.
+    The LED0 flashes for 25 seconds. An interrupt occurs when DIO0 receives an
+    appropriate digital signal and the signal trigger the interrupt conditions.
+    Press the button BTN0 to trigger the interrupt within 25 seconds. The
+    program calls irq_handler, which makes the LED1 flashes for 3 seconds,
+    when the interrupt occurs.
 """
 import time
 import academicIO
@@ -62,11 +65,11 @@ def irq_handler():
         for x in range(0, 5):
             # turn LED0 on
             LED.write(led, led_on)
-            # add a short delay before acquiring next data point
+            # add a short delay
             time.sleep(1)
             # turn LED0 off
             LED.write(led, led_off)
-            # add a short delay before acquiring next data point
+            # add a short delay
             time.sleep(1)
 
 # specify the DIO channel that serves as the interrupt channel
@@ -81,7 +84,10 @@ with academicIO.DIIRQ(irq_channel) as DI_IRQ:
     # specify whether to register an interrupt on the rising edge or the
     # falling edge of the digital input signal. To register an interrupt on
     # the rising edge of the digital input signal, set interrupt_type_rising
-    # as True and interrupt_type_falling as False
+    # as True and interrupt_type_falling as False. The possible combination is
+    # shown as indicated in this table:
+    #     interrupt_type_rising    True    False   True
+    #     interrupt_type_falling   False   True    True
     interrupt_type_rising = True
     interrupt_type_falling = False
     # specify the number of edges of the signal that must occur for this
