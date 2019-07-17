e8eb70f72c24750be087ec210e65b5c6aa74fae4
Yawei Hu <37615956+yawhuu@users.noreply.github.com>
2019-07-17 16:13:19 +0800

    change import path (#12)

--- examples/digital/PWM.py
+++ examples/digital/PWM.py
@@ -20,7 +20,7 @@ Result:
 """
 import os
 import sys
-sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))), 'source/nielvisiii'))
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), 'source/nielvisiii'))
 
 import time
 import academicIO
