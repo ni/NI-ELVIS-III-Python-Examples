79f7c3a837b2cbe3cbc33ea419f282ad6d734d35
Yawei Hu <37615956+yawhuu@users.noreply.github.com>
2019-08-05 16:06:54 +0800

    [Test]Refactor AO tests with Python unittest framework (#15)
    ### What
    Refactor the AO tests. Does not add any new tests in this pull request.
    
    ### How
    Refactor using [Python unittest framework](https://docs.python.org/3/library/unittest.html). Split all existing tests into different test cases. 
    
    Use `python -m unittest tests/AO.py` to run the test.
    
    ### Tests
    - Test_AnalogOutput_WriteSingleChannel
        - test_WriteSinglePoint_ReturnExpectedReadBack
        - test_WriteSinglePoint_ReturnDifferentValueFromEachChannel
    - Test_AnalogOutput_WriteMultipleChannels
        - test_WriteSinglePoint_ReturnExpectedReadBack
    - Test_AnalogOutput_Assertion
        - test_OpenWithInvalidBank_ShowAssertion

--- tests/AO.py
+++ tests/AO.py
@@ -3,6 +3,7 @@ Hardware setup:
     1. Connect AI0 and AO0 on connector A.
     2. Connect AI3 and AO1 on connector A.
 """
+import unittest
 import time
 import pytest
 import sys
@@ -11,44 +12,61 @@ import academicIO
 from enums import *
 
 bank = Bank.A
-AO_single_channel = academicIO.AnalogOutput({'bank': bank, 'channel': AOChannel.AO0})
-AO_multiple_channels = academicIO.AnalogOutput({'bank': bank, 'channel': AOChannel.AO0}, 
-                                                         {'bank': bank, 'channel': AOChannel.AO1})
-AI_multiple_channels = academicIO.AnalogInput({'bank': bank, 'channel': AIChannel.AI0},
-                                                        {'bank': bank, 'channel': AIChannel.AI3})
 
-input_value = 3.5
-AO_multiple_channels.write(input_value)
-value_array = AI_multiple_channels.read()
-for value in value_array:
-    print value,
-    assert value == pytest.approx(input_value, 0.1)
-print 
+class Test_AnalogOutput_WriteSingleChannel(unittest.TestCase):
+    def setUp(self):
+        self.AO_single_channel = academicIO.AnalogOutput({'bank': bank, 'channel': AOChannel.AO0})
+        self.AI_single_channel = academicIO.AnalogInput({'bank': bank, 'channel': AIChannel.AI0})
 
-input_new_value = 2.0       
-AO_single_channel.write(input_new_value)
-value_array = AI_multiple_channels.read()
-for index, value in enumerate(value_array):
-    print value,
-    if index == 0:
-        assert value == pytest.approx(input_new_value, 0.1)
-    else:
-        assert value == pytest.approx(input_value, 0.1)
-print
+    def tearDown(self):
+        self.AO_single_channel.close()
+        self.AI_single_channel.close()
 
-input_value = 0.5
-AO_multiple_channels.write(input_value)
-value_array = AI_multiple_channels.read()
-for value in value_array:
-    print value,
-    assert value == pytest.approx(input_value, 0.1)
-print
+    def test_WriteSinglePoint_ReturnExpectedReadBack(self):
+        input_value = 2.0       
+        self.AO_single_channel.write(input_value)
+        value_array = self.AI_single_channel.read()
+        for value in value_array:
+            self.assertEqual(value, pytest.approx(input_value, 0.1))
 
-AO_single_channel.close()
-AO_multiple_channels.close()
-AI_multiple_channels.close()
+    def test_WriteSinglePoint_ReturnDifferentValueFromEachChannel(self):
+        AO_multiple_channels = academicIO.AnalogOutput({'bank': bank, 'channel': AOChannel.AO0}, 
+                                                       {'bank': bank, 'channel': AOChannel.AO1})
+        AI_multiple_channels = academicIO.AnalogInput({'bank': bank, 'channel': AIChannel.AI0},
+                                                      {'bank': bank, 'channel': AIChannel.AI3})
+        input_value_for_single_channels = 2.5
+        input_value_for_multiple_channels = 3.3
+        AO_multiple_channels.write(input_value_for_multiple_channels)
+        self.AO_single_channel.write(input_value_for_single_channels)
+        value_array = AI_multiple_channels.read()
+        for index, value in enumerate(value_array):
+            if index == 0:
+                self.assertEqual(value, pytest.approx(input_value_for_single_channels, 0.1))
+            else:
+                self.assertEqual(value, pytest.approx(input_value_for_multiple_channels, 0.1))
 
-try:
-    academicIO.AnalogOutput({'bank': bank, 'channel': 2})
-except (AssertionError) as err:
-    print "Caught the error - The channels available in the AO should be 0-1."
+        AO_multiple_channels.close()
+        AI_multiple_channels.close()
+
+class Test_AnalogOutput_WriteTwoChannels(unittest.TestCase):
+    def setUp(self):
+        self.AO_multiple_channels = academicIO.AnalogOutput({'bank': bank, 'channel': AOChannel.AO0}, 
+                                                            {'bank': bank, 'channel': AOChannel.AO1})
+        self.AI_multiple_channels = academicIO.AnalogInput({'bank': bank, 'channel': AIChannel.AI0},
+                                                           {'bank': bank, 'channel': AIChannel.AI3})
+    
+    def tearDown(self):
+        self.AO_multiple_channels.close()
+        self.AI_multiple_channels.close()
+
+    def test_WriteSinglePoint_ReturnExpectedReadBack(self):
+        input_value = 3.5
+        self.AO_multiple_channels.write(input_value)
+        value_array = self.AI_multiple_channels.read()
+        for value in value_array:
+            self.assertEqual(value, pytest.approx(input_value, 0.1))
+
+class Test_AnalogOutput_Assertion(unittest.TestCase):
+    def test_OpenWithInvalidBank_ShowAssertion(self):
+        with self.assertRaises(AssertionError):
+            academicIO.AnalogOutput({'bank': bank, 'channel': 2})
