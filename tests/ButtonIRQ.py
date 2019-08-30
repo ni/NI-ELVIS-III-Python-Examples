e98be76d81a128e2943ff1d5486a7671af6a48af
Yawei Hu <37615956+yawhuu@users.noreply.github.com>
2019-08-30 13:46:25 +0800

    [Button IRQ]Refactor tests (#33)
    ### What
    1. Use `import unittest` and `import nielvis`. 
    
    ### Tests
    - Test_ButtonIRQ
        - test_OpenAndWait_DoesnotShowError
     - Test_ButtonIRQ_OpenAssertion
        - test_OpenWithInvalidInterruptType_ShowAssertion
        - test_OpenWithInvalidIrqNumber_ShowAssertion
        - test_OpenWithInvalidTimeout_ShowAssertion
        - test_OpenWithInvalidEdgeCount_ShowAssertion

--- tests/ButtonIRQ.py
+++ tests/ButtonIRQ.py
@@ -2,97 +2,87 @@
 Output:
     The interrupt event occurs when the button is clicked; otherwise timeout.
     The callback_function function is called when the interrupt occured.
-"""					 
-import time
-import sys
-sys.path.append('source/nielvisiii')
-import academicIO
-from enums import *
+"""	
+import unittest		 
+from nielvis import ButtonIRQ, IRQNumber
 
 def irq_handler():
     print("Button interrupt is triggered. Now it is callback time.")
 
-irq_number = IRQNumber.IRQ1
-timeout= 6000
-interrupt_type_rising = True
-interrupt_type_falling = False
-edge_count = 2
-with academicIO.ButtonIRQ(irq_handler,
-                          irq_number,
-                          timeout,
-                          interrupt_type_rising,
-                          interrupt_type_falling,
-                          edge_count) as Button_IRQ:
-    Button_IRQ.wait()
+class Test_ButtonIRQ(unittest.TestCase):
+    def test_OpenAndWait_DoesnotShowError(self):
+        testcases = [
+            {'irq_number': IRQNumber.IRQ1, 'timeout': 6000, 'interrupt_type_rising': True, 'interrupt_type_falling': False, 'edge_count': 2},
+            {'irq_number': IRQNumber.IRQ1, 'timeout': 6000, 'interrupt_type_rising': False, 'interrupt_type_falling': True, 'edge_count': 2},
+            {'irq_number': IRQNumber.IRQ1, 'timeout': 6000, 'interrupt_type_rising': True, 'interrupt_type_falling': True, 'edge_count': 2},
+        ]
 
-interrupt_type_rising = False
-interrupt_type_falling = True
-with academicIO.ButtonIRQ(irq_handler,
-                          irq_number,
-                          timeout,
-                          interrupt_type_rising,
-                          interrupt_type_falling,
-                          edge_count) as Button_IRQ:
-    Button_IRQ.wait()
+        for testcase in testcases:
+            with ButtonIRQ(irq_handler,
+                           testcase['irq_number'],
+                           testcase['timeout'],
+                           testcase['interrupt_type_rising'],
+                           testcase['interrupt_type_falling'],
+                           testcase['edge_count'],) as button_irq:
+                button_irq.wait()
 
-interrupt_type_rising = True
-interrupt_type_falling = True
-with academicIO.ButtonIRQ(irq_handler,
-                          irq_number,
-                          timeout,
-                          interrupt_type_rising,
-                          interrupt_type_falling,
-                          edge_count) as Button_IRQ:
-    Button_IRQ.wait()
-try:
-    interrupt_type_rising = False
-    interrupt_type_falling = False
-    academicIO.ButtonIRQ(irq_handler,
-                            irq_number,
-                            timeout,
-                            interrupt_type_rising,
-                            interrupt_type_falling,
-                            edge_count)
-except AssertionError:
-    print("Caught the error - rising and falling interrupt type can't be false at the same time.")
+class Test_ButtonIRQ_OpenAssertion(unittest.TestCase):
+    def test_OpenWithInvalidInterruptType_ShowAssertion(self):
+        testcases = [
+            {'irq_number': IRQNumber.IRQ1, 'timeout': 6000, 'interrupt_type_rising': False, 'interrupt_type_falling': False, 'edge_count': 2}
+        ]
 
-try:
-    with academicIO.ButtonIRQ(irq_handler,
-                          0,
-                          timeout,
-                          interrupt_type_rising,
-                          interrupt_type_falling,
-                          edge_count) as Button_IRQ:
-        Button_IRQ.wait()
-except AssertionError:
-    print("Caught the error - 0 can't be irq_number of ButtonIRQ.")
+        for testcase in testcases:
+            with self.assertRaises(AssertionError):
+                button_irq = ButtonIRQ(irq_handler,
+                                       testcase['irq_number'],
+                                       testcase['timeout'],
+                                       testcase['interrupt_type_rising'],
+                                       testcase['interrupt_type_falling'],
+                                       testcase['edge_count'])
 
-try:
-    academicIO.ButtonIRQ(irq_handler,
-                         irq_number,
-                         -1,
-                         interrupt_type_rising,
-                         interrupt_type_falling,
-                         edge_count)
-except AssertionError:
-    print("Caught the error - the timeout value should be greater than 0.")
+    def test_OpenWithInvalidIrqNumber_ShowAssertion(self):
+        invalid_irq_number = 0
+        testcases = [
+            {'irq_number': invalid_irq_number, 'timeout': 6000, 'interrupt_type_rising': False, 'interrupt_type_falling': False, 'edge_count': 2}
+        ]
 
-try:
-    academicIO.ButtonIRQ(irq_handler,
-                         irq_number,
-                         timeout,
-                         interrupt_type_rising,
-                         interrupt_type_falling,
-                         0)
-except AssertionError:
-    print("Caught the error - the number of edges of the signal should be greater than 0.")
+        for testcase in testcases:
+            with self.assertRaises(AssertionError):
+                button_irq = ButtonIRQ(irq_handler,
+                                       testcase['irq_number'],
+                                       testcase['timeout'],
+                                       testcase['interrupt_type_rising'],
+                                       testcase['interrupt_type_falling'],
+                                       testcase['edge_count'])
 
-try:
-    academicIO.ButtonIRQ(irq_handler,
-                         irq_number,
-                         timeout,
-                         interrupt_type_rising,
-                         interrupt_type_falling,
-                         4294967296)
-except AssertionError:
-    print("Caught the error - the number of edges of the signal should be less than 4294967296.")
+    def test_OpenWithInvalidTimeout_ShowAssertion(self):
+        invalid_timeout = -1
+        testcases = [
+            {'irq_number': IRQNumber.IRQ1, 'timeout': invalid_timeout, 'interrupt_type_rising': False, 'interrupt_type_falling': False, 'edge_count': 2}
+        ]
+
+        for testcase in testcases:
+            with self.assertRaises(AssertionError):
+                button_irq = ButtonIRQ(irq_handler,
+                                       testcase['irq_number'],
+                                       testcase['timeout'],
+                                       testcase['interrupt_type_rising'],
+                                       testcase['interrupt_type_falling'],
+                                       testcase['edge_count'])
+
+    def test_OpenWithInvalidEdgeCount_ShowAssertion(self):
+        invalid_edge_counts = [0, 4294967296]
+        testcases = [
+            {'irq_number': IRQNumber.IRQ1, 'timeout': 6000, 'interrupt_type_rising': False, 'interrupt_type_falling': False, 'edge_count': invalid_edge_counts[0]},
+            {'irq_number': IRQNumber.IRQ1, 'timeout': 6000, 'interrupt_type_rising': False, 'interrupt_type_falling': False, 'edge_count': invalid_edge_counts[1]},
+        ]
+
+        for testcase in testcases:
+            with self.assertRaises(AssertionError):
+                button_irq = ButtonIRQ(irq_handler,
+                                       testcase['irq_number'],
+                                       testcase['timeout'],
+                                       testcase['interrupt_type_rising'],
+                                       testcase['interrupt_type_falling'],
+                                       testcase['edge_count'])
