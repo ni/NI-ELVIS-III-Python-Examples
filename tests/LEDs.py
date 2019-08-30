8e98917c50a2b815d41ac19d9c057380a5c2e009
Yawei Hu <37615956+yawhuu@users.noreply.github.com>
2019-08-30 13:51:06 +0800

    [LED]Refactor tests (#38)
    ### What
    1. Use `import unittest` and `import nielvis`. 
    
    ### Tests
    - Test_Led
        - test_Write_DoesnotShowError

--- tests/LEDs.py
+++ tests/LEDs.py
@@ -3,26 +3,26 @@ Output:
   LED0 turns on and then LED1, LED2, LED3,
   LED0 turns off and then LED2, LED2, LED3
 """
+import unittest
 import time
-import sys
-sys.path.append('source/nielvisiii')
-import academicIO
-from enums import *
+from nielvis import LEDs, Led
 
-with academicIO.LEDs() as LED:       
-    LED.write()						        # The default values are - led: led0, value_to_set = True.
-    time.sleep(1)
-    LED.write(led=Led.LED1)			    # The default 'value_to_set' is True
-    time.sleep(1)
-    LED.write(Led.LED2, True)
-    time.sleep(1)
-    LED.write(Led.LED3, True)
-    time.sleep(1)
-    LED.write(value_to_set=False)	# the default 'led' is 'led0'
-    time.sleep(1)
-    LED.write(Led.LED1, False)
-    time.sleep(1)
-    LED.write(Led.LED2, False)
-    time.sleep(1)
-    LED.write(Led.LED3, False)
-    time.sleep(1)
+class Test_Led(unittest.TestCase):
+  def test_Write_DoesnotShowError(self):
+    with LEDs() as LED:       
+        LED.write()                         # The default values are - led: led0, value_to_set = True.
+        time.sleep(1)
+        LED.write(led=Led.LED1)             # The default 'value_to_set' is True
+        time.sleep(1)
+        LED.write(Led.LED2, True)
+        time.sleep(1)
+        LED.write(Led.LED3, True)
+        time.sleep(1)
+        LED.write(value_to_set=False)       # the default 'led' is 'led0'
+        time.sleep(1)
+        LED.write(Led.LED1, False)
+        time.sleep(1)
+        LED.write(Led.LED2, False)
+        time.sleep(1)
+        LED.write(Led.LED3, False)
+        time.sleep(1)
