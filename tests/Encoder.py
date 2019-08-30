bf672b27070098afc2623bb2007a93c4364dd682
Yawei Hu <37615956+yawhuu@users.noreply.github.com>
2019-08-30 13:48:14 +0800

    [Encoder]Refactor test (#36)
    ### What
    1. Use `import unittest` and `import nielvis`. 
    
    ### Tests
    - Test_Encoder
        - test_ReadWithQuadratureMode_DoesnotShowError
        - test_ReadWithStepAndDirectionMode_DoesnotShowError

--- tests/Encoder.py
+++ tests/Encoder.py
@@ -1,42 +1,39 @@
 """
 Hardware setup:
-	1. Connect connector A phase A (DIO18) to ENC.A of ADXL345.
-	2. Connect connector A phase B (DIO19) to ENC.B of ADXL345.
+	1. Connect connector A phase A (DIO18) to ENC.A (CLK) of the key.
+	2. Connect connector A phase B (DIO19) to ENC.B (DL) of the key.
 """
 import time
-import sys
-sys.path.append('source/nielvisiii')
-import academicIO
-from enums import *
+import unittest
+from nielvis import Encoder, EncoderChannel, EncoderMode, Bank
 
 bank = Bank.A
 channel = EncoderChannel.ENC9
-with academicIO.Encoder(bank, channel, EncoderMode.QUADRATURE) as encoder_quadrature_mode:
-    reset_counter = True
-    encoder_quadrature_mode.read(reset_counter)
-    reset_counter = False
-    for x in range(0, 5):
-        counter_value, counter_direction_decrease = encoder_quadrature_mode.read(reset_counter)
-        print(counter_value)
+
+class Test_Encoder(unittest.TestCase):
+    def __print(self, counter_value, counter_direction_decrease):
+        print(counter_value, end = '')
         if counter_direction_decrease:
-        	print(", direction is decreasing")
+            print(", direction is decreasing")
         else:
-        	print(", direction is increasing")
+            print(", direction is increasing")
 
-        time.sleep(1)
-
-print("done")
-
-with academicIO.Encoder(bank, channel, EncoderMode.STEP_AND_DIRECTION) as encoder_step_mode:
-    reset_counter = True
-    encoder_step_mode.read(reset_counter)
-    reset_counter = False
-    for x in range(0, 5):
-        counter_value, counter_direction_decrease = encoder_step_mode.read(reset_counter)
-        print(counter_value)
-        if counter_direction_decrease:
-        	print(", direction is decreasing")
-        else:
-        	print(", direction is increasing")
-
-        time.sleep(1)
+    def test_ReadWithQuadratureMode_DoesnotShowError(self):
+        with Encoder(bank, channel, EncoderMode.QUADRATURE) as encoder_quadrature_mode:
+            reset_counter = True
+            encoder_quadrature_mode.read(reset_counter)
+            reset_counter = False
+            for x in range(0, 5):
+                counter_value, counter_direction_decrease = encoder_quadrature_mode.read(reset_counter)
+                self.__print(counter_value, counter_direction_decrease)
+                time.sleep(1)
+    
+    def test_ReadWithStepAndDirectionMode_DoesnotShowError(self):
+        with Encoder(bank, channel, EncoderMode.STEP_AND_DIRECTION) as encoder_step_mode:
+            reset_counter = True
+            encoder_step_mode.read(reset_counter)
+            reset_counter = False
+            for x in range(0, 5):
+                counter_value, counter_direction_decrease = encoder_step_mode.read(reset_counter)
+                self.__print(counter_value, counter_direction_decrease)
+                time.sleep(1)
