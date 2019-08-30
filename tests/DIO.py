c76c2548539d6d5e98d5d86d59b8f6edec21b6a6
Yawei Hu <37615956+yawhuu@users.noreply.github.com>
2019-08-30 13:47:40 +0800

    [Digital Input Ouput]Refactor tests (#35)
    ### What
    1. Use `import unittest` and `import nielvis`. 
    
    ### Tests
    - Test_DigitalInputOuput
        - test_WriteAndReadFourChannels
        - test_WriteAndReadTwoChannels
        - test_WriteAndReadSingleChannel
    - Test_DigitalInputOuput_Assertion
        - test_writeInvalidValue_ShowAssertion
        - test_writeInvalidChannel_ShowAssertion
        - test_readInvalidChannel_ShowAssertion

--- tests/DIO.py
+++ tests/DIO.py
@@ -5,48 +5,59 @@ Hardware setup:
   1. Connect DIO4 and DIO11 on connector A.
   2. Connect DIO8 and DIO12 on connector A.
 """
-import time
-# import pytest
-import sys
-sys.path.append('source/nielvisiii')
-import academicIO
-from enums import *
+import unittest
+from nielvis import DigitalInputOutput, Bank, DIOChannel
 
 bank = Bank.A
-with academicIO.DigitalInputOutput(bank,
+
+class Test_DigitalInputOuput(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.value = True
+        self.dio = DigitalInputOutput(bank,
                                    [DIOChannel.DIO2, DIOChannel.DIO3, DIOChannel.DIO4, DIOChannel.DIO8,
-                                    DIOChannel.DIO9, DIOChannel.DIO10, DIOChannel.DIO11, DIOChannel.DIO12]) as DIO:
-    value = True
+                                    DIOChannel.DIO9, DIOChannel.DIO10, DIOChannel.DIO11, DIOChannel.DIO12])
 
-    # multiple channels
-    DIO.write(value, [DIOChannel.DIO2, DIOChannel.DIO3, DIOChannel.DIO4, DIOChannel.DIO8])
-    data = DIO.read([DIOChannel.DIO9, DIOChannel.DIO10, DIOChannel.DIO11, DIOChannel.DIO12])
-    print(data)
-    assert data == [1, 1, 1, 1]
+    @classmethod
+    def tearDownClass(self):
+        self.dio.close()
 
-    # a single channel
-    DIO.write(not value, [DIOChannel.DIO3])
-    data = DIO.read([DIOChannel.DIO10])
-    print(data)
-    assert data == [0]
+    def test_WriteAndReadFourChannels(self):
+        self.dio.write(self.value, [DIOChannel.DIO2, DIOChannel.DIO3, DIOChannel.DIO4, DIOChannel.DIO8])
+        data = self.dio.read([DIOChannel.DIO9, DIOChannel.DIO10, DIOChannel.DIO11, DIOChannel.DIO12])
+        self.assertEqual(data, [1, 1, 1, 1])
 
-    # multiple channels
-    DIO.write(value, [DIOChannel.DIO2, DIOChannel.DIO4])
-    data = DIO.read([DIOChannel.DIO9, DIOChannel.DIO11])
-    print(data)
-    assert data == [1, 1]
+    def test_WriteAndReadTwoChannels(self):
+        self.dio.write(self.value, [DIOChannel.DIO2, DIOChannel.DIO4])
+        data = self.dio.read([DIOChannel.DIO9, DIOChannel.DIO11])
+        self.assertEqual(data, [1, 1])
 
-    try:
-        DIO.write(200, [DIOChannel.DIO1])
-    except (AssertionError) as err:
-        print("Caught the error - Only True or False can be written to DI.")
+    def test_WriteAndReadSingleChannel(self):
+        self.dio.write(not self.value, [DIOChannel.DIO3])
+        data = self.dio.read([DIOChannel.DIO10])
+        self.assertEqual(data, [0])
 
-    try:
-        DIO.write(value, [20])
-    except (AssertionError) as err:
-        print("Caught the error - The channels available in the DIO should be 0-19.")
+class Test_DigitalInputOuput_Assertion(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.value = True
+        self.channel = [DIOChannel.DIO1]
+        self.dio = DigitalInputOutput(bank,
+                                   [DIOChannel.DIO2, DIOChannel.DIO3, DIOChannel.DIO4, DIOChannel.DIO8,
+                                    DIOChannel.DIO9, DIOChannel.DIO10, DIOChannel.DIO11, DIOChannel.DIO12])
 
-    try:
-        DIO.read([20])
-    except (AssertionError) as err:
-        print("Caught the error - The channels available in the DIo should be 0-19.")
+    @classmethod
+    def tearDownClass(self):
+        self.dio.close()
+
+    def test_writeInvalidValue_ShowAssertion(self):
+        with self.assertRaises(AssertionError):
+            self.dio.write(200, self.channel)
+
+    def test_writeInvalidChannel_ShowAssertion(self):
+        with self.assertRaises(AssertionError):
+            self.dio.write(self.value, [20])
+
+    def test_readInvalidChannel_ShowAssertion(self):
+        with self.assertRaises(AssertionError):
+            self.dio.read([20])
