057d48460818e249c5adfdc6bceae2b26460b4f7
Yawei Hu <37615956+yawhuu@users.noreply.github.com>
2019-08-30 13:52:49 +0800

    [Timer IRQ]Refactor tests (#41)
    ### What
    1. Use `import unittest` and `import nielvis`. 
    
    ### Tests
    - Test_TimerIRQ
        - test_WaitFor5s_DoesnotShowError

--- tests/TimerIRQ.py
+++ tests/TimerIRQ.py
@@ -3,15 +3,15 @@ Output:
     The interrupt event occurs based on the time interval; otherwise timeout.
     The irq_handler function is called when the interrupt occured.
 """
-import time
-import sys
-sys.path.append('source/nielvisiii')
-import academicIO
-from enums import *
+import unittest
+
+from nielvis import TimerIRQ
 
 def irq_handler():
     print("Timer interrupt is triggered. Now it is callback time.")
 
-irq_interval= 5000000    # 5s
-with academicIO.TimerIRQ(irq_handler, irq_interval) as Timer_IRQ:
-    Timer_IRQ.wait()
+class Test_TimerIRQ(unittest.TestCase):
+    def test_WaitFor5s_DoesnotShowError(self):
+        irq_interval= 5000000    # 5s
+        with TimerIRQ(irq_handler, irq_interval) as Timer_IRQ:
+            Timer_IRQ.wait()
