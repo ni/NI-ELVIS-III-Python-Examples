2b1ed6b168608ed340aab91f13a0fed6044c14d1
Yawei Hu <37615956+yawhuu@users.noreply.github.com>
2019-08-30 13:48:43 +0800

    [I2C]Refactor tests (#37)
    ### What
    1. Use `import unittest` and `import nielvis`. 
    
    ### Tests
    - Test_I2C
        - test_readWithStandardMode
        - test_readWithFastMode

--- tests/I2C.py
+++ tests/I2C.py
@@ -3,28 +3,25 @@ Hardware setup:
   1. Connect DIO15 to I2C.SDA of ADXL345.
   2. Connect DIO14 to I2C.SCL of ADXL345.
 """
-import time
-import sys
-sys.path.append('source/nielvisiii')
-import academicIO
-from enums import *
+import unittest
+from nielvis import I2C, Bank, I2CSpeedMode
 
 bank = Bank.A
 slave_device_address = 0x53
 data_to_write = [0x00, 0x80]
 number_bytes_to_read = 1
 
-with academicIO.I2C(bank, I2CSpeedMode.STANDARD) as I2C_standard_mode:  
-    I2C_standard_mode.write(slave_device_address, data_to_write)
-    
-    return_value = I2C_standard_mode.read(slave_device_address, number_bytes_to_read)
-    print(return_value)
-    assert hex(return_value[0]) == '0xe5'
+class Test_I2C(unittest.TestCase):
+    def test_readWithStandardMode(self):
+        with I2C(bank, I2CSpeedMode.STANDARD) as I2C_standard_mode:  
+            I2C_standard_mode.write(slave_device_address, data_to_write)
+            
+            return_value = I2C_standard_mode.read(slave_device_address, number_bytes_to_read)
+            self.assertEqual(hex(return_value[0]), '0xe5')
 
-with academicIO.I2C(bank, I2CSpeedMode.FAST) as I2C_fast_mode:  
-    I2C_fast_mode.write(slave_device_address, data_to_write)
-    
-    return_value = I2C_fast_mode.read(slave_device_address, number_bytes_to_read)
-    print(return_value)
-    print(hex(return_value[0]))
-    assert hex(return_value[0]) == '0xe5'
+    def test_readWithFastMode(self):
+        with I2C(bank, I2CSpeedMode.FAST) as I2C_fast_mode:  
+            I2C_fast_mode.write(slave_device_address, data_to_write)
+            
+            return_value = I2C_fast_mode.read(slave_device_address, number_bytes_to_read)
+            self.assertEqual(hex(return_value[0]), '0xe5')
