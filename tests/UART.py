0d5817e3b628975c9d3299710c101cfa97e91bb9
Yawei Hu <37615956+yawhuu@users.noreply.github.com>
2019-08-30 13:53:15 +0800

    [UART]Refactor tests (#42)
    ### What
    1. Use `import unittest` and `import nielvis`. 
    
    ### Tests
    - Test_UART
        - test_WriteAndRead1Byte_ReturnExpectedValue

--- tests/UART.py
+++ tests/UART.py
@@ -1,23 +1,33 @@
 """
+Use GY25 chip.
 Hardware setup:
 	1. Connect connector A UART.RX (DIO16) to UART.TX of a device.
 	2. Connect connector A UART.TX (DIO17) to UART.RX of a device.
 """
 import time
-import sys
-sys.path.append('source/nielvisiii')
-import academicIO
-from enums import *
+import unittest
+from nielvis import UART, Bank, UARTBaudRate, UARTDataBits, UARTStopBits, UARTParity
 
 bank = Bank.A
-baud_rate = UARTBaudRate.RATE9600
+baud_rate = UARTBaudRate.RATE115200
 data_bits = UARTDataBits.BITS8
 stop_bits = UARTStopBits.ONE
 parity = UARTParity.NO
-with academicIO.UART(bank,baud_rate, data_bits, stop_bits, parity) as uart:
-    value = 'Hello World'
-    uart.write(value)
 
-    bytes_to_read = 10
-    return_value = uart.read(bytes_to_read)
-    print(return_value)
+class Test_UART(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.uart = UART(bank,baud_rate, data_bits, stop_bits, parity)
+
+    @classmethod
+    def tearDownClass(self):
+        self.uart.close()
+
+    def test_WriteAndRead1Byte_ReturnExpectedValue(self):
+        self.uart.write(b'\xA5')
+        self.uart.write(b'\x51')
+        time.sleep(0.1)
+
+        bytes_to_read = 1
+        return_value = self.uart.read(bytes_to_read)
+        self.assertEqual(return_value, b'\xAA')
